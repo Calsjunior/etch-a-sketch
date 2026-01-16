4a417d989b4f417e28e9d16a0ca0a03f56e07cc6
cal <sakphea05@gmail.com>
2026-01-16 10:08:48 +0700

    feat: create a grid based on given size

---
+++ script.js
@@ -0,0 +1,15 @@
+function makeGrid(size) {
+    const gridContainer = document.querySelector("#grid-container");
+    for (let i = 0; i < size; i++) {
+        const column = document.createElement("div");
+        column.classList.add("grid-column");
+        for (let j = 0; j < size; j++) {
+            const row = document.createElement("div");
+            row.classList.add("grid-row");
+            column.appendChild(row);
+        }
+        gridContainer.appendChild(column);
+    }
+}
+
+makeGrid(16);
