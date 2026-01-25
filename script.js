925074ac9c9eaf8e0562d5249eb5571acbf3f08d
cal <sakphea05@gmail.com>
2026-01-25 20:34:34 +0700

    feat: add switching between color picker and rainbow mode

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 let size = 0;
+let currentColorMode = "COLOR";
+
 const inputGridSize = document.querySelector(".grid-size");
 const addSizeButton = document.querySelector("button");
 const gridContainer = document.querySelector(".grid-container");
 const displayGridSize = document.querySelector(".display-size");
+const colorMode = document.querySelector(".color-mode");
+const rainbowMode = document.querySelector(".rainbow-mode");
 const colorInput = document.querySelector(".color-picker");
 colorInput.value = "";
 
 function makeGrid(size) {
+    gridContainer.textContent = "";
+
     for (let i = 0; i < size; i++) {
         const column = document.createElement("div");
         column.classList.add("grid-column");
@@ -19,9 +25,30 @@ function makeGrid(size) {
     }
 }
 
+function randomRgb() {
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+colorMode.addEventListener("click", (event) => {
+    if (event.target.classList.contains("color-picker")) {
+        currentColorMode = "COLOR";
+    } else if (event.target.classList.contains("rainbow-mode")) {
+        currentColorMode = "RAINBOW";
+    }
+});
+
 gridContainer.addEventListener("mouseover", (event) => {
     if (event.target.classList.contains("grid-row")) {
-        event.target.style.backgroundColor = colorInput.value;
+        if (currentColorMode == "COLOR") {
+            event.target.style.backgroundColor = colorInput.value;
+        } else if (currentColorMode == "RAINBOW" && !event.target.style.backgroundColor) {
+            // Prevents redrawing on already drawn color only in rainbow mode
+            event.target.style.backgroundColor = randomRgb();
+            rainbowMode.style.backgroundColor = event.target.style.backgroundColor;
+        }
     }
 });
 
@@ -35,7 +62,6 @@ addSizeButton.addEventListener("click", (event) => {
     size = inputGridSize.value;
     displayGridSize.textContent = `${inputGridSize.value} x ${inputGridSize.value}`;
     inputGridSize.value = "";
-    gridContainer.textContent = "";
 
     makeGrid(size);
 });
