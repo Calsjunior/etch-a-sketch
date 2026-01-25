eaa6fe3d963d2d0de9f6372a4b87a28c2f1e739a
cal <sakphea05@gmail.com>
2026-01-25 18:18:06 +0700

    feat: allow picking colors from browser picker

--- script.js
+++ script.js
@@ -1,23 +1,9 @@
 let size = 0;
-const input = document.querySelector(".grid-size");
-const button = document.querySelector("button");
+const inputGridSize = document.querySelector(".grid-size");
+const addSizeButton = document.querySelector("button");
 const gridContainer = document.querySelector(".grid-container");
-const displaySize = document.querySelector(".display-size");
-button.addEventListener("click", (event) => {
-    event.preventDefault();
-    if (input.value > 100) {
-        alert("Size cannot be above 100");
-        return;
-    }
-    size = input.value;
-
-    displaySize.textContent = `${input.value} x ${input.value}`;
-    input.value = "";
-    gridContainer.textContent = "";
-
-    makeGrid(size);
-    hoverGrid();
-});
+const displayGridSize = document.querySelector(".display-size");
+const colorInput = document.querySelector(".color-picker");
 
 function makeGrid(size) {
     for (let i = 0; i < size; i++) {
@@ -32,11 +18,23 @@ function makeGrid(size) {
     }
 }
 
-function hoverGrid() {
-    const gridContainer = document.querySelector(".grid-container");
-    gridContainer.addEventListener("mouseover", (event) => {
-        if (event.target.classList.contains("grid-row")) {
-            event.target.classList.add("hovered");
-        }
-    });
-}
+gridContainer.addEventListener("mouseover", (event) => {
+    if (event.target.classList.contains("grid-row")) {
+        event.target.style.backgroundColor = colorInput.value;
+    }
+});
+
+addSizeButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    if (inputGridSize.value > 100) {
+        alert("Size cannot be above 100");
+        return;
+    }
+    size = inputGridSize.value;
+
+    displayGridSize.textContent = `${inputGridSize.value} x ${inputGridSize.value}`;
+    inputGridSize.value = "";
+    gridContainer.textContent = "";
+
+    makeGrid(size);
+});
